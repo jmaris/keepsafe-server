7d3091d1edf7d2e675e71d9d9d1afb4b8922825d
Liam Jack <cuonic@cuonic.com>
2017-08-05 01:44:47 +0200

    Falcon API ok

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import falcon
+
 Base = declarative_base()
 engine = create_engine('mysql+pymysql://keepsafe:7k77xJuGeqZYu97gpFfS8xsheXYHGHjB@localhost/keepsafe_db')
 
@@ -14,13 +16,28 @@ class User(Base):
 
 # Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind = engine)
-session = Session()
+#Session = sessionmaker(bind = engine)
+#session = Session()
 
-new_user = User(
-    username_hash = 'f08MWDBZh8BFMDFxxap1TT/geuP8jeBqN7bQsToN1usxrYyhipONKAuh3mmJZ4+eyThKh3oea8d8bk0lrQj7Uw==',
-    public_key = 'QOK6kY3xFFT/4wQhjkCZrprclu5VrKc/gdK5PYdI6kQ='
-)
+#new_user = User(
+#    username_hash = 'f08MWDBZh8BFMDFxxap1TT/geuP8jeBqN7bQsToN1usxrYyhipONKAuh3mmJZ4+eyThKh3oea8d8bk0lrQj7Uw==',
+#    public_key = 'QOK6kY3xFFT/4wQhjkCZrprclu5VrKc/gdK5PYdI6kQ='
+#)
 
-session.add(new_user)
-session.commit()
+#session.add(new_user)
+#session.commit()
+
+class UserResource(object):
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = 'User::Get'
+
+    def on_post(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = 'User::Post'
+
+app = falcon.API()
+
+userResource = UserResource()
+
+app.add_route('/users', userResource)
